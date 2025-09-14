92928bf049fcdf0e377b770c3e703ff1a98a8dee
Tristan Gagne <tristan@salmar.com>
2025-09-13 22:14:04 -0400

    bug fixes

--- app.py
+++ app.py
@@ -465,3 +465,4 @@ def explore_decks():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3000, debug=True)
+print("haisidiiasiii i the forijoasiuoo99299999999999999999999999999999999999999999999999999996")
